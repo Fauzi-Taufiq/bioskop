097ba82bd503e7b733a88993a62c7cf7175c711d
rizkicahya-ramdani <rizki.cahya03@gmail.com>
2024-06-10 02:32:59 +0700

    nambahin koneksi ke db (rizki)

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername 	= "localhost";
+$username 		= "root";
+$password 		= "";
+$database 		= "db_bioskop";
+
+$connection = mysqli_connect($servername, $username, $password, $database);
+
+if (!$connection) {
+	die("Connection gagal! " . $connection->connect_error);
+} else {
+	echo "Connection berhasil!";
+}
