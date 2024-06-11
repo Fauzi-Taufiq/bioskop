b69ad058b265aab45dbaa2f1cea0acfc44c42e79
rizkicahya-ramdani <rizki.cahya03@gmail.com>
2024-06-11 12:52:50 +0700

    misahin index (rizki)

--- admin/login.php
+++ admin/login.php
@@ -12,12 +12,11 @@
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
-                    <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                        <div class="col-lg-6">
+                    <div class="row d-flex justify-content-center align-items-center">
+                        <div class="col-lg-10">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
                                 </div>
                                 <form class="user">
                                     <div class="form-group">
@@ -36,7 +35,7 @@
                                                 Me</label>
                                         </div>
                                     </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                    <a href="index.html" class="btn btn-info btn-user btn-block">
                                         Login
                                     </a>
                                     <hr>
