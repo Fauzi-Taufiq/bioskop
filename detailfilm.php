0f187867269465e9d80b35bfff2f29d40f779b51
unknown <trisnajuliant86@gmail.com>
2024-06-24 16:20:08 +0700

    mengisi detail film

--- detailfilm.php
+++ detailfilm.php
@@ -91,14 +91,13 @@
                   <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/></svg>
                 </div>
                 <div class="col p-4 d-flex flex-column position-static">
-                  <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
-                  <h3 class="mb-0">Featured post</h3>
-                  <div class="mb-1 text-body-secondary">Nov 12</div>
-                  <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                  <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                    Continue reading
-                    <svg class="bi"><use xlink:href="#chevron-right"/></svg>
-                  </a>
+                  <h3 class="mb-3">Transformers</h3>
+                  <div class="mb-3 text-body-light">4.8</div>
+                  <p class="card-text mb-3">An ancient struggle between two Cybertronian races, the heroic Autobots and the evil Decepticons, comes to Earth, with a clue to the ultimate power held by a teenager.</p>
+                  <p>
+                    Director :
+                    Michael Bay
+                  </p>
                 </div>
               </div>
             </div>
